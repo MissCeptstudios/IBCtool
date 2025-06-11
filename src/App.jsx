edac9de4cf16e34e198b89ac4c68ecd60e928af8
MissCeptstudios <joanna.cholas@gmail.com>
2025-06-11 21:29:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import IBCtool from './IBCtool.jsx'
 
 export default function App() {
-  console.log('App is rendering!')
-  return (
-    <div>
-      <h1>DEBUG: App is working</h1>
-      <IBCtool />
-    </div>
-  )
+  return <IBCtool />
 }
