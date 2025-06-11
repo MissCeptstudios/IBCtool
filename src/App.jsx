c00854f0b9cbb5df7baa3e3d7fde599e13850d77
MissCeptstudios <joanna.cholas@gmail.com>
2025-06-11 21:12:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
+import IBCtool from './IBCtool.jsx'
+
 export default function App() {
-  return (
-    <div>
-      <h1>IB Calculator Tool</h1>
-      <p>Calculator is working!</p>
-    </div>
-  )
-}
+  return
